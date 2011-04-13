13e02a40756dbdbe4417046c7449077848c61739
Andrew Davidson <andrew@amdavidson.com>
2011-04-13 00:41:36 +0000

    temporary commit to move files.

--- email_check.rb
+++ email_check.rb
@@ -2,6 +2,7 @@
 
 require 'net/pop'
 require 'openssl'
+require 'rubygems'
 
 username = ENV['ECUSER']
 password = ENV['ECPASS']
@@ -12,9 +13,23 @@ Net::POP3.start('pop.gmail.com', 995, username, password) do |pop|
 		puts 'No mail.'
 	else
 		pop.each_mail do |mail|
-			p mail.header
-		end
+            
+      if mail.pop =~ /From: Andrew Davidson <andrew@amdavidson.com>/
+        d = Date.parse(mail.pop.scan(/Date: (\w.*)/).to_s)   
+        t = mail.pop.scan(/Subject: (\w.*)/).to_s.downcase.sub!(/\s/,"-")
+        puts "Year: " + d.year.to_s
+        puts "Month: " + d.month.to_s
+        puts "Title: " + t
+        filename = "messages/" + d.year.to_s + "-" + d.month.to_s + "-" + t
+        puts "Saving post as " + filename
+        File.open(filename, 'w') { |f| f.write(mail.pop) }
+      else
+        puts "Not from Andrew."
+      end
+
+    end
 	end
 end
+
 
 
